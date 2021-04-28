4d692ca04cafdd06cd0f22685e3df507182f440a
jpdesenvolvedor <joao.paulo@atoscapital.com.br>
2021-04-28 18:18:53 -0300

    Criação da pasta de Controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const AdmController = require('../app/controllers/adm_controller');
+const HomeController = require('../app/controllers/home_controller');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', HomeController.index);
+router.get('/adm.json', AdmController.index);
 
 module.exports = router;
